2ea74cae926063c89e8cdc99772f00451d7fa117
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2022-11-08 05:10:51 -0600

    update App.tsx to include Header and Socials

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './styles/main.scss';
 import Header from './components/layout/Header/Header';
+import Socials from './components/utilities/Socials/Socials';
 
 function App() {
-  return <Header backgroundColor='bg-color__light--accent-dark' />;
+  return (
+    <>
+      <Header backgroundColor='bg-color__light--accent-dark' />
+      <Socials type='sticky' backgroundColor='bg-color__light--accent-dark' />
+    </>
+  );
 }
 
 export default App;
