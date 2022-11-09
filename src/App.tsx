ce1a21a1af8589fc86e8f7170efa7caa123f8b8d
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2022-11-09 08:54:38 -0600

    🔨UPDATE 'App' component.
    Add Switch for light/dark mode switching.
    Add ThemeContext for appropriate color switching.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './styles/main.scss';
+import ThemeContext from './store/theme-context';
 import Header from './components/layout/Header/Header';
 import Socials from './components/utilities/Socials/Socials';
 import Footer from './components/layout/Footer/Footer';
+import Switch from './components/utilities/Switch/Switch';
 
 function App() {
+  const { isDarkMode } = useContext(ThemeContext);
+
+  const headerFooterBGColor = isDarkMode
+    ? 'bg-color__dark--accent-dark'
+    : 'bg-color__light--accent-dark';
+
   return (
     <>
-      <Header backgroundColor='bg-color__light--accent-dark' />
-      <Socials type='sticky' backgroundColor='bg-color__light--accent-dark' />
-      <Footer backgroundColor='bg-color__light--accent-dark' />
+      <Header backgroundColor={headerFooterBGColor} />
+      <Socials type='sticky' backgroundColor={headerFooterBGColor} />
+      <Switch />
+      <Footer backgroundColor={headerFooterBGColor} />
     </>
   );
 }
