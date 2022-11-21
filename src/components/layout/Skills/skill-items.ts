3398a44e6dd8d1a42575d69b58c237ae90ff7f24
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2022-11-21 17:08:44 -0600

    ➕ ADD: 'Skills' Layout Component.

---
+++ src/components/layout/Skills/skill-items.ts
@@ -0,0 +1,26 @@
+export const FRONT_END_SKILLS = [
+  'HTML',
+  'CSS',
+  'Sass%(CSS Preprocessor)',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'Frontity%(React framework)',
+  'WordPress%(WP Rest API)',
+]; // uses % for string manipulation in Skills.tsx
+
+export const TOOLS_AND_DESIGN_SKILLS = [
+  'Jest',
+  'Git',
+  'VS Code',
+  'Adobe XD',
+];
+
+export const OTHER_SKILLS = [
+  'Vercel',
+  'Netlify',
+  'React Testing Library',
+  'Landing Page Design',
+  'UI/UX Design',
+];
