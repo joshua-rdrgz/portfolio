02b385970c5bba16f08df3c4ab0ade0c8032fb6c
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2022-12-08 04:30:41 -0600

    ⚒️REFACTOR: 'Herobox' Component.
    Refactor styling to not rely on viewport height.
    Refactor Component info structure.

--- src/components/layout/Herobox/Herobox.tsx
+++ src/components/layout/Herobox/Herobox.tsx
@@ -3,7 +3,9 @@ import useStandardMQueries from '../../../hooks/useStandardMQueries';
 import ThemeContext from '../../../store/theme-context';
 import Heading from '../../utilities/Heading/Heading';
 import Icon from '../../utilities/Icon/Icon';
+import IconType from '../../utilities/Icon/IconType';
 import Button from '../../utilities/Button/Button';
+import BtnType from '../../utilities/Button/ButtonType';
 import Wrapper from '../../Wrapper';
 import HeadingType from '../../utilities/Heading/HeadingType';
 
@@ -13,52 +15,80 @@ const Herobox: React.FC = () => {
   const { biggerThanSmallPhone, biggerThanBigPhone, biggerThanBigDesktop } =
     useStandardMQueries();
 
-  const HEADING_COLOR = isDarkMode
-    ? 'color__dark--accent'
-    : 'color__light--accent-medium';
-
-  const HEADING_SIZE = biggerThanSmallPhone
-    ? biggerThanBigPhone
-      ? biggerThanBigDesktop
-        ? 'heading-size__1'
-        : 'heading-size__2'
-      : 'heading-size__3--bold'
-    : 'heading-size__4';
-
-  const SUBHEADING: HeadingType['subHeading'] = {
-    type: 'both',
-    textUpper: {
-      content: `Hi 👋🏽 I'm`,
-      headingSize: biggerThanBigPhone
+  const HEADING_PROPS: HeadingType = {
+    Tag: 'h1',
+    headingSize: biggerThanSmallPhone
+      ? biggerThanBigPhone
         ? biggerThanBigDesktop
-          ? 'heading-size__4'
-          : 'heading-size__5'
-        : 'heading-size__6',
-      textColor: isDarkMode ? 'color--black-5' : 'color--black-75',
+          ? 'heading-size__1'
+          : 'heading-size__2'
+        : 'heading-size__3--bold'
+      : 'heading-size__4',
+    headingColor: isDarkMode
+      ? 'color__dark--accent'
+      : 'color__light--accent-medium',
+    subHeading: {
+      type: 'both',
+      textUpper: {
+        content: `Hi 👋🏽 I'm`,
+        headingSize: biggerThanBigPhone
+          ? biggerThanBigDesktop
+            ? 'heading-size__4'
+            : 'heading-size__5'
+          : 'heading-size__6',
+        textColor: isDarkMode ? 'color--black-5' : 'color--black-75',
+      },
+      textLower: {
+        content: (
+          <Wrapper>
+            A passionate{' '}
+            <strong
+              className={`color__${isDarkMode ? 'dark' : 'light'}--accent`}
+            >
+              Front End Developer
+            </strong>
+            , a lifelong{' '}
+            <strong
+              className={`color__${isDarkMode ? 'dark' : 'light'}--accent`}
+            >
+              learner
+            </strong>
+            , and a motivated{' '}
+            <strong
+              className={`color__${isDarkMode ? 'dark' : 'light'}--accent`}
+            >
+              self-starter
+            </strong>
+            .
+          </Wrapper>
+        ),
+        textSize: biggerThanBigPhone
+          ? 'text-size--xxlarge'
+          : 'text-size--large',
+        textColor: isDarkMode ? 'color--black-15' : 'color--black-75',
+      },
     },
-    textLower: {
-      content: (
-        <Wrapper>
-          A passionate{' '}
-          <strong className={`color__${isDarkMode ? 'dark' : 'light'}--accent`}>
-            Front End Developer
-          </strong>
-          , a lifelong{' '}
-          <strong className={`color__${isDarkMode ? 'dark' : 'light'}--accent`}>
-            learner
-          </strong>
-          , and a motivated{' '}
-          <strong className={`color__${isDarkMode ? 'dark' : 'light'}--accent`}>
-            self-starter
-          </strong>
-          .
-        </Wrapper>
-      ),
-      textSize: biggerThanBigDesktop
-        ? 'text-size--xxlarge'
-        : 'text-size--large',
-      textColor: isDarkMode ? 'color--black-15' : 'color--black-75',
+    headingClassName: 'herobox__header',
+    wrapperClassName: 'herobox__title',
+  };
+
+  const BUTTON_PROPS: BtnType = {
+    Tag: 'a',
+    href: '#projects',
+    btnInfo: {
+      type: 'primary',
+      textColor: isDarkMode ? 'color--black-75' : 'color--black-5',
+      btnColor: `bg-color__${isDarkMode ? 'dark' : 'light'}--accent`,
+      hoverBgColor: `hover-bg-color__${isDarkMode ? 'dark' : 'light'}--accent`,
+      main: true,
     },
+  };
+
+  const ICON_PROPS: IconType = {
+    type: 'memoji',
+    svg: `./assets/memoji/${isDarkMode ? 'dark' : 'light'}/memoji.svg`,
+    wrappingClassName: 'herobox__memoji',
+    alt: 'Josh hard at work coding behind his computer!',
   };
 
   return (
@@ -68,38 +98,10 @@ const Herobox: React.FC = () => {
       }--gradient-hero`}
     >
       <div className='herobox__content'>
-        <Heading
-          Tag='h1'
-          headingSize={HEADING_SIZE}
-          headingColor={HEADING_COLOR}
-          subHeading={SUBHEADING}
-          headingClassName='herobox__header'
-        >
-          Joshua Rodriguez.
-        </Heading>
-        <Button
-          Tag='a'
-          href='#projects'
-          btnInfo={{
-            type: 'primary',
-            textColor: isDarkMode ? 'color--black-75' : 'color--black-5',
-            btnColor: `bg-color__${isDarkMode ? 'dark' : 'light'}--accent`,
-            hoverBgColor: `hover-bg-color__${
-              isDarkMode ? 'dark' : 'light'
-            }--accent`,
-            main: true,
-          }}
-        >
-          See My Work
-        </Button>
+        <Heading {...HEADING_PROPS}>Joshua Rodriguez.</Heading>
+        <Button {...BUTTON_PROPS}>See My Work</Button>
       </div>
-      <Icon
-        type='memoji'
-        svg={`./assets/memoji/${isDarkMode ? 'dark' : 'light'}/memoji.svg`}
-        wrappingClassName='herobox__memoji'
-        imgClassName='herobox__memoji-img'
-        alt='Josh hard at work coding behind his computer!'
-      />
+      <Icon {...ICON_PROPS} />
     </section>
   );
 };
