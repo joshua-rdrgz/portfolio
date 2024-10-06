31a1bd82b19a9e3b333ac23f3104605fb8cb9661
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-10-06 02:23:47 -0500

    make alt required in image field

--- src/sanity/schemaTypes/postType.ts
+++ src/sanity/schemaTypes/postType.ts
@@ -30,11 +30,19 @@ export const postType = defineType({
         hotspot: true,
       },
       fields: [
-        {
+        defineField({
           name: 'alt',
           type: 'string',
           title: 'Alternative text',
-        },
+          validation: (rule) =>
+            rule.custom((value, context) => {
+              const parent = context?.parent as { asset?: { _ref?: string } };
+
+              return !value && parent?.asset?._ref
+                ? 'Alt text is required when an image is present'
+                : true;
+            }),
+        }),
       ],
     }),
     defineField({
