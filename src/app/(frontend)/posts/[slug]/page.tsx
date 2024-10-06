36aaf92d6995d09f56f4551a3df5a1b61c3aa306
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-10-06 02:23:52 -0500

    add image to post page

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,7 +1,8 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import { client } from '@/sanity/lib/client';
 import { POST_QUERY } from '@/sanity/lib/queries';
-import { notFound } from 'next/navigation';
-import Link from 'next/link';
+import { urlFor } from '@/sanity/lib/image';
 
 type PostIndexProps = { params: { slug: string } };
 
@@ -10,12 +11,22 @@ const options = { next: { revalidate: 60 } };
 export default async function Page({ params }: PostIndexProps) {
   const post = await client.fetch(POST_QUERY, params, options);
 
-  if (!post) {
-    notFound();
-  }
-
   return (
-    <main className='container mx-auto grid grid-cols-1 gap-6 p-12'>
+    <main className='container mx-auto grid grid-cols-1 gap-6 py-12'>
+      {post?.mainImage ? (
+        <Image
+          className='w-full aspect-[800/400]'
+          src={urlFor(post.mainImage)
+            .width(800)
+            .height(400)
+            .quality(80)
+            .auto('format')
+            .url()}
+          alt={post?.mainImage?.alt || ''}
+          width='800'
+          height='400'
+        />
+      ) : null}
       <h1 className='text-4xl font-bold text-balance'>{post?.title}</h1>
       <hr />
       <Link href='/posts'>&larr; Return to index</Link>
