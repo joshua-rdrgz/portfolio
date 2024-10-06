562cf57ae99830a053f750ec19a41cf234c260fb
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-10-06 03:38:56 -0500

    add tailwindcss typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,8 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/sanity/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {},
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
 export default config;
