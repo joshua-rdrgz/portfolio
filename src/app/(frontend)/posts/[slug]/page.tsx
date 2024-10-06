8115037fb716b65e44271ca7171482e18f00a42c
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-10-06 03:39:11 -0500

    add PortableText for block content rendering

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { PortableText } from 'next-sanity';
+import { components } from '@/sanity/portableTextComponents';
 import { client } from '@/sanity/lib/client';
 import { POST_QUERY } from '@/sanity/lib/queries';
 import { urlFor } from '@/sanity/lib/image';
@@ -28,6 +30,11 @@ export default async function Page({ params }: PostIndexProps) {
         />
       ) : null}
       <h1 className='text-4xl font-bold text-balance'>{post?.title}</h1>
+      {post?.body ? (
+        <div className='prose'>
+          <PortableText value={post?.body} components={components} />
+        </div>
+      ) : null}
       <hr />
       <Link href='/posts'>&larr; Return to index</Link>
     </main>
