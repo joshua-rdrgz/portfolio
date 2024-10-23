4d39e1b1cf24a91c8d861ec5b2256272bb2fca24
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-10-23 15:07:22 -0500

    update Herobox styling

--- src/components/layout/portfolio/Herobox.tsx
+++ src/components/layout/portfolio/Herobox.tsx
@@ -17,14 +17,26 @@ export default function Herobox({ data }: HeroboxProps) {
   return (
     <section
       id={data.navItemRef?.slug!}
-      className='min-h-screen md:min-h-[85vh] relative flex flex-col md:flex-row'
+      className='h-[85vh] relative flex flex-col justify-center -mx-7'
     >
-      <div className='md:w-2/3 flex flex-col justify-center items-center text-center md:items-start md:text-left p-8 md:pl-16'>
-        <div className='text-md md:text-lg lg:text-xl'>{data.prefix}</div>
-        <div className='text-4xl md:text-6xl lg:text-7xl py-4'>
-          {data.header}
+      <div className='absolute inset-0'>
+        <AppImage
+          src={data.heroboxImage?.src}
+          alt={data.heroboxImage?.alt}
+          priority
+          wrapperClassName='w-full h-full'
+          className='object-cover w-full h-full'
+        />
+        <div className='absolute inset-0 bg-background/90'></div>
+      </div>
+      <div className='relative z-[1] text-center md:text-left p-8 md:pl-16'>
+        <div className='text-md md:text-lg lg:text-xl text-foreground'>
+          {data.prefix}
         </div>
-        <div className='text-lg md:text-xl lg:text-2xl max-w-lg'>
+        <h2 className='text-4xl md:text-6xl lg:text-7xl py-4 text-foreground'>
+          {data.header}
+        </h2>
+        <div className='text-lg md:text-xl lg:text-2xl max-w-lg text-foreground'>
           <PortableText
             value={data.tagline!}
             components={{
@@ -36,15 +48,6 @@ export default function Herobox({ data }: HeroboxProps) {
             }}
           />
         </div>
-      </div>
-      <div className='hidden md:block md:w-1/3 relative'>
-        <AppImage
-          src={data.heroboxImage?.src}
-          alt={data.heroboxImage?.alt}
-          priority
-          wrapperClassName='w-full h-full absolute top-0 right-0 transform rotate-20'
-          className='object-cover object-center w-full h-full'
-        />
       </div>
     </section>
   );
