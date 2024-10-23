d606ae6d5dda99d832e863620262d1203434b12d
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-10-23 15:09:10 -0500

    add bottom border to mobile header

--- src/components/layout/sidebar/MobileSidebar.tsx
+++ src/components/layout/sidebar/MobileSidebar.tsx
@@ -23,7 +23,7 @@ export default function MobileSidebar({ sidebarData }: MobileSidebarProps) {
   const linkProperties = theSidebarData.linkListProperties;
 
   return (
-    <header className='lg:hidden sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary'>
+    <header className='lg:hidden sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary  border-b'>
       <div className='mx-4 sm:mx-8 flex h-14 items-center'>
         <div className='flex items-center space-x-4 lg:space-x-0'>
           <SH.Root open={isOpen} onOpenChange={setIsOpen}>
